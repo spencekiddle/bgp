3a64131ac5f4dc3a8b92761953981f91d3e5052c
Spence Kiddle <spence@netx.net>
2016-09-16 21:36:00 -0700

    indentation

--- test/piece.js
+++ test/piece.js
@@ -1,14 +1,14 @@
 (function(QUnit) {
 	
-    QUnit.module('BGP.Piece');
+	QUnit.module('BGP.Piece');
 
-    QUnit.test('Piece', function(assert) {
-        assert.expect(4);
-        var piece = new BGP.Piece();
-        assert.ok(piece instanceof BGP.Piece, '`piece` is an instance of BGP.Piece');
-        assert.equal(piece.get('name'), 'new piece', 'piece instance is called `new piece`');
-        assert.equal(piece.get('color'), 'black', 'piece is a black');
-        assert.equal(piece.get('movement'), 1, 'piece can move 1 place');
-    });
+	QUnit.test('Piece', function(assert) {
+		assert.expect(4);
+		var piece = new BGP.Piece();
+		assert.ok(piece instanceof BGP.Piece, '`piece` is an instance of BGP.Piece');
+		assert.equal(piece.get('name'), 'new piece', 'piece instance is called `new piece`');
+		assert.equal(piece.get('color'), 'black', 'piece is a black');
+		assert.equal(piece.get('movement'), 1, 'piece can move 1 place');
+	});
 
 })(QUnit);
